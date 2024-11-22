9b05a20e270986f9afc5fb52ae7cdc6197b2cb28
Manu <vfcmanikandan7@gmail.com>
2024-11-22 16:55:36 +0530

    updated path

--- Backend/model/dbConnection.js
+++ Backend/model/dbConnection.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
      try{
         console.log('working  connectDB')
         
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/FoodKo', {});
+        const conn = await mongoose.connect('mongodb+srv://vfcmanikandan7:H3QX5P5e0HRSbmnG@foodko-cluster.uqlcj.mongodb.net/FoodKo?retryWrites=true&w=majority&appName=FoodKo-Cluster', {});
 
 
        
